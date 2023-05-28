4fbecc13ca689055342e81143ad6a4c64b2d3c24
766rsr <766rsr@gmail.com>
2023-05-28 17:27:58 +0800

    新增電子發票掃描與部分布局優化

--- MyApplication/app/src/main/java/com/example/myapplication/SignUp.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SignUp.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -28,15 +29,26 @@ class SignUp : AppCompatActivity() {
         but.setOnClickListener {
             val email = findViewById<EditText>(R.id.editTextTextEmailAddress_signup).text.toString()
             val password = findViewById<EditText>(R.id.editTextTextPassword_signup).text.toString()
+            val reenterpassword = findViewById<EditText>(R.id.editTextTextPassword_signup_reenter).text.toString()
 
-            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
-                if (it.isSuccessful){
-                    Log.d("Test","成功註冊")
-                    finish()
-                }else{
-                    Log.w("Test","註冊失敗", it.exception) //it.exception是把錯誤原因記下來
-                    showMessage("註冊會員失敗")
+            if(email.isNullOrEmpty()){
+                Toast.makeText(this, "請輸入信箱", Toast.LENGTH_SHORT).show()
+            }else if(password.isNullOrEmpty()){7
+                Toast.makeText(this, "請輸入密碼", Toast.LENGTH_SHORT).show()
+            }else if(reenterpassword.isNullOrEmpty()){
+                Toast.makeText(this, "請重新輸入密碼", Toast.LENGTH_SHORT).show()
+            }else if(password == reenterpassword){
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Log.d("Test","成功註冊")
+                        finish()
+                    }else{
+                        Log.w("Test","註冊失敗", it.exception) //it.exception是把錯誤原因記下來
+                        showMessage("註冊會員失敗")
+                    }
                 }
+            }else{
+                Toast.makeText(this, "密碼輸入不一致", Toast.LENGTH_SHORT).show()
             }
         }
     }
