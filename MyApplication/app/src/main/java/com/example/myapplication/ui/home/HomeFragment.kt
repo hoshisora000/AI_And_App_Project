5b4f2b8a833d12edfbfc454f8d2744b915fdc922
766rsr <766rsr@gmail.com>
2023-06-03 17:39:12 +0800

    布局優化 條碼實作

--- MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -12,11 +12,14 @@ import com.example.myapplication.R
 
 //-------------------
 import android.content.Intent
+import android.graphics.Bitmap
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.example.myapplication.MainActivity
 import com.example.myapplication.SignUp
@@ -28,6 +31,9 @@ import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseUser
 import com.google.gson.Gson
 import com.google.gson.JsonObject
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.oned.Code128Writer
+import com.google.zxing.oned.Code39Writer
 import okhttp3.*
 import java.io.IOException
 
@@ -41,6 +47,7 @@ class HomeFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
 
     private val client = OkHttpClient()
+    private var membercodabar = ""
     private val url_query_member = "https://hoshisora000.lionfree.net/api/query_member.php"
 
     override fun onCreateView(
@@ -70,6 +77,18 @@ class HomeFragment : Fragment() {
             mainActivity.re_data_invoice()
         }
 
+        _binding!!.layoutCodabarBt.setOnClickListener {
+            if(membercodabar == ""){
+                showToast("尚未設定手機載具")
+            }else{
+                if(_binding!!.layoutCodabar.visibility == View.GONE){
+                    _binding!!.layoutCodabar.visibility = View.VISIBLE
+                }else{
+                    _binding!!.layoutCodabar.visibility = View.GONE
+                }
+            }
+        }
+
         _binding!!.butLogin.setOnClickListener {
             val email = _binding!!.editTextTextEmailAddress.text.toString()
             val password = _binding!!.editTextTextPassword.text.toString()
@@ -94,6 +113,92 @@ class HomeFragment : Fragment() {
             }
         }
 
+        _binding!!.layoutMemberBt.setOnClickListener {
+            if(_binding!!.layoutMember.visibility == View.GONE){
+                _binding!!.textMemberNickname.setText(_binding!!.textMember.text.substring(0,_binding!!.textMember.text.length-3))
+                _binding!!.textMemberCode.setText(membercodabar)
+                _binding!!.layoutMember.visibility = View.VISIBLE
+            }else{
+                _binding!!.layoutMember.visibility = View.GONE
+            }
+        }
+
+        _binding!!.btLayoutMember.setOnClickListener {
+            if(_binding!!.textMemberNickname.text.toString() == ""){
+                showToast("請輸入暱稱")
+            }else{
+                Thread{
+                    val mainActivity = activity as MainActivity
+                    mainActivity.progressbar()
+
+                    val formBody = FormBody.Builder()
+                        .add("uid", Firebase.auth.currentUser?.uid.toString())
+                        .add("nickname", _binding!!.textMemberNickname.text.toString())
+                        .add("mobile_barcode", _binding!!.textMemberCode.text.toString() )
+                        .build()
+
+                    println("777 "+_binding!!.textMemberCode.text.toString())
+
+                    OkHttpClient().newCall(Request.Builder()
+                        .url("https://hoshisora000.lionfree.net/api/update_member_inf.php")
+                        .post(formBody)
+                        .build()).enqueue(object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
+                            e.printStackTrace()
+                        }
+                        override fun onResponse(call: Call, response: Response) {
+                            if (response.isSuccessful) {
+                                val responseBody = response.body?.string()
+                                println(responseBody)
+
+                                getmember()
+                            } else {
+                                println("Request failed")
+                            }
+                        }
+                    })
+
+                    Thread.sleep(1000)
+                    requireActivity().runOnUiThread {
+                        _binding!!.layoutMember.visibility =View.GONE
+                        _binding!!.textMember.setText(_binding!!.textMemberNickname.text.toString()+" 您好")
+                    }
+                    mainActivity.progressbar()
+                }.start()
+            }
+        }
+
+        _binding!!.layoutMemberpasswordBt.setOnClickListener {
+            if(_binding!!.layoutMemberpassword.visibility == View.GONE){
+                _binding!!.layoutMemberpassword.visibility = View.VISIBLE
+            }else{
+                _binding!!.layoutMemberpassword.visibility = View.GONE
+            }
+        }
+
+        _binding!!.btLayoutMemberpassword.setOnClickListener {
+            Thread{
+                val mainActivity = activity as MainActivity
+                mainActivity.progressbar()
+                Thread.sleep(1000)
+                requireActivity().runOnUiThread {
+                    _binding!!.layoutMemberpassword.visibility =View.GONE
+                }
+                mainActivity.progressbar()
+            }.start()
+        }
+
+        _binding!!.layoutAppupdateBt.setOnClickListener{
+            showToast("您已擁有最新版本")
+        }
+
+        _binding!!.layoutAppverBt.setOnClickListener{
+            showToast("Ver 1.0.0")
+        }
+
+        _binding!!.layoutInfoBt.setOnClickListener{
+            showToast("            -製作者-\nCHIH-HSUAN WANG\n      YOU-CHEN LIN ")
+        }
         return root
     }
 
@@ -135,7 +240,7 @@ class HomeFragment : Fragment() {
                             .get("nickname")
                             .asString
 
-                        val mobile_barcode = jsonObject
+                        membercodabar = jsonObject
                             .getAsJsonArray("data")[0]
                             .asJsonObject
                             .get("mobile_barcode")
@@ -145,6 +250,7 @@ class HomeFragment : Fragment() {
                             _binding!!.textMember.setText(nickname+" 您好")
                         }
 
+                        set_member_codabar(membercodabar)
                     }catch (e:Exception){
 
                     }
@@ -163,5 +269,33 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        val scale = resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    private fun set_member_codabar(value: String) {
+        val barcodeColor = ContextCompat.getColor(requireContext(), R.color.black)
+        val backgroundColor = ContextCompat.getColor(requireContext(), android.R.color.white)
+
+        val bitMatrix = Code39Writer().encode(value,BarcodeFormat.CODE_39,dpToPx(300),dpToPx(80))
+
+        val pixels = IntArray(bitMatrix.width * bitMatrix.height)
+        for (y in 0 until bitMatrix.height) {
+            val offset = y * bitMatrix.width
+            for (x in 0 until bitMatrix.width) {
+                pixels[offset + x] =
+                    if (bitMatrix.get(x, y)) barcodeColor else backgroundColor
+            }
+        }
+        val bitmap = Bitmap.createBitmap(bitMatrix.width,bitMatrix.height,Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels,0,bitMatrix.width,0,0,bitMatrix.width,bitMatrix.height)
+
+        requireActivity().runOnUiThread{
+            _binding!!.imageBarcode.setImageBitmap(bitmap)
+            _binding!!.textBarcodeNumber.text = value
+        }
     }
 }
