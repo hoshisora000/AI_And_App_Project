ae5feeb39d5190d81d71f60c9874b54048838982
hoshisora000 <hoshisora000@gmail.com>
2023-06-12 13:48:02 +0800

    更新投影片

--- web/add_number.php
+++ web/add_number.php
@@ -54,74 +54,49 @@ if ($_SESSION["level"] != 9) {
                 <div class="row gx-5 justify-content-center">
                     <!-- Pricing card free-->
                     <!---內容加在裡面-->
-                    <div class="container my-5">
-                        <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-                            <div class="col-md-10 mx-auto col-lg-5">
-                                <div class="p-5 mb-4 bg-light rounded-3">
-                                    <h3 class="display-4 fw-bold lh-1">新增發票號碼</h3>
-                                    <div class="container-fluid py-5">
-                                        <form class="p-4 p-md-5 border rounded-3 bg-light" name="form1" id="form1"
-                                            action="" method="POST" enctype='multipart/form-data'>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="item_id" id="item_id"
-                                                    size="20" placeholder="item_id" value="">
-                                                <label for="floatingInput">期數(1120506)</label>
-                                            </div>
-
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="item_neme" size="20"
-                                                    placeholder="item_neme" value="">
-                                                <label for="floatingInput">商品名稱</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control price_change" id="price" type="text"
-                                                    name="item_price" size="20" placeholder="item_price" value="">
-                                                <label for="floatingInput">商品原價</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control price_change" id="discount" type="text"
-                                                    name="item_disc" size="20" placeholder="item_disc" value="1.0">
-                                                <label for="floatingInput">商品折數</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" id="d_price" name="item_d_price"
-                                                    size="20" placeholder="item_d_price" value="" readonly=" readonly">
-                                                <label for="floatingInput">商品售價</label>
-                                                <!--JS算-->
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="item_title" size="20"
-                                                    placeholder="item_title" value="">
-                                                <label for="floatingInput">商品標題</label>
-                                            </div>
-
-                                            <div class="form-floating">
-                                                <textarea class="form-control" placeholder="Leave a comment here"
-                                                    name="item_inf" id="item_inf" style="height: 200px"></textarea>
-                                                <label for="floatingTextarea2">商品說明</label>
-                                            </div>
-                                            <br>
-                                            <div class="mb-3">
-                                                <h4>商品狀態</h4>
-                                                <input type="radio" name="state" value="0">上架中
-                                                <input type="radio" name="state" value="3">缺貨中
-                                                <input type="radio" name="state" value="9">已下架
-                                            </div>
-                                            <div class="mb-3">
-                                                <h4>更新商品圖片</h4>
-                                                <input type='file' accept=".png,.jpg" name="item_photo" id="item_photo">
-                                            </div>
-                                            <button class="btn btn-primary btn-lg px-4 me-md-2"
-                                                type="submit">送出</button>
-                                            <a href="./item_set.php" class="btn btn-warning btn-lg px-4">取消</a>
-                                        </form>
-
+                    <div class="col-md-10 mx-auto col-lg-5">
+                        <div class="p-5 mb-4 bg-light rounded-3">
+                            <h3 class="display-4 fw-bold lh-1">新增發票號碼</h3>
+                            <div class="container-fluid py-5">
+                                <form class="p-4 p-md-5 border rounded-3 bg-light" name="form1" id="form1" action="./add_number.php"
+                                    method="POST" enctype='multipart/form-data'>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" type="text" name="item_id" size="20"
+                                            placeholder="per" value="">
+                                        <label for="floatingInput">發票期數</label>
                                     </div>
-                                </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" type="text" name="item_neme" size="20"
+                                            placeholder="ssp" value="">
+                                        <label for="floatingInput">特別獎</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control price_change" id="price" type="text" name="sp"
+                                            size="20" placeholder="item_price" value="">
+                                        <label for="floatingInput">特獎</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control price_change" id="discount" type="text" name="head1"
+                                            size="20" placeholder="item_disc" value="">
+                                        <label for="floatingInput">頭獎1</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control price_change" id="discount" type="text" name="head2"
+                                            size="20" placeholder="item_disc" value="">
+                                        <label for="floatingInput">頭獎2</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control price_change" id="discount" type="text" name="head3"
+                                            size="20" placeholder="item_disc" value="">
+                                        <label for="floatingInput">頭獎3</label>
+                                    </div>
+
+                                    <button class="btn btn-primary btn-lg px-4 me-md-2" type="submit">送出</button>
+                                    <a href="./admin.php" class="btn btn-warning btn-lg px-4">取消</a>
+                                </form>
 
                             </div>
                         </div>
-                        <div class="col-md-3"></div>
 
                     </div>
 
@@ -153,3 +128,28 @@ if ($_SESSION["level"] != 9) {
 </body>
 
 </html>
+
+<?php if(isset($_POST['per'])&&isset($_POST['ssp'])&&isset($_POST['sp'])){ 
+    $per = $_POST['per'];
+    $ssp = $_POST['ssp'];
+    $sp = $_POST['sp'];
+    $head1 = $_POST['head1'];
+    $head2 = $_POST['head2'];
+    $head3 = $_POST['head3'];
+
+    $severname = "192.168.2.200"; //SQL位置
+    $username = "hoshiso1_system"; //帳號
+    $password = "system123456"; //密碼
+    $dbname = "hoshiso1_project"; //SQL名稱
+    $link = mysqli_connect($severname, $username, $password, $dbname); // 建立MySQL的資料庫連結
+
+    $sql = "INSERT INTO `winning_numbers`('$per','$ssp','$sp','$head1','$head2','$head3')"; //SQL新增資料
+    if ($result = mysqli_query($link, $sql)) // 送出查詢的SQL指令
+        $msg = "<span style='color:#0000FF'>資料新增成功!</span>";
+    else
+        $msg = "<span style='color:#FF0000'>資料新增失敗！<br>錯誤代碼：" . mysqli_errno($link) . "<br>錯誤訊息：" . mysqli_error($link) . "</span>";
+    mysqli_close($link); // 關閉資料庫連結
+
+
+
+} ?>
